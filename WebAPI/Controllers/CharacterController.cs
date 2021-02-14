372b43c2459f9dd253a3c2ca05419a2e653a0ac9
Rajat Misra <rajatmisra23@gmail.com>
2021-02-15 01:54:11 +0530

    Commiting character controller with initial four methods

---
+++ WebAPI/Controllers/CharacterController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>()
+        {
+            new Character(),
+            new Character{Id = 1,Name = "Bilbo"}
+        };
+
+        [HttpGet("GetAll")]
+        public IActionResult GetCharacter()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("GetOne")]
+        public IActionResult GetSingleCharacter()
+        {
+            return Ok(characters.FirstOrDefault());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCharacterById(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult CreateCharacter(Character character)
+        {
+            characters.Add(character);
+            return Ok(characters);
+        }
+    }
+}
